a824ade23577296fdf9421312e6b0a07af875f94
Adoraxuu <adoraxu01@gmail.com>
2023-12-10 13:39:29 +0800

    add product list

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def product_cover(product, variant = :cover_sm)
+    if product.cover.attached?
+      image_tag product.cover.variant(variant)
+    else
+      image_tag "/images/products/default.jpg"
+    end
+  end
 end
